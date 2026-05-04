831971fac1eee7d9922d0bd9bbce0fdbb0b8e8b0
Evan Simbajon <200720255@my.xu.edu.ph>
2026-05-04 22:01:20 +0800

    add: added progress tracker with debug buttons for testing

--- src/pages/lgu/dashboard-u.jsx
+++ src/pages/lgu/dashboard-u.jsx
@@ -2,16 +2,45 @@ import React, { useState } from 'react';
 import '../../css/udashboard.css';
 import hamIcon from '../../assets/hamburger.svg';
 import logo from '../../assets/logo.svg';
+import addCircleIcon from '../../assets/add-circle.svg';
+import dashboardIcon from '../../assets/dashboard.svg';
 
 export default function Udashboard() {
+
+  /* Side Bar Functionality */
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
     const toggleSidebar = () => {
         setIsSidebarOpen(!isSidebarOpen);
     };
 
+    /* Progress Bar Functionality */
+    const [currentStep, setCurrentStep] = useState(1);
+    const progressWidth = ((currentStep - 1) / (4 - 1)) * 100;
+    const totalSteps = 4;
+
+    /* Dev Button Functionality for Testing */
+    const handleNext = () => {
+        if (currentStep < totalSteps) {
+            setCurrentStep(prev => prev + 1);
+        }
+    };
+
+    const handlePrev = () => {
+        if (currentStep > 1) {
+            setCurrentStep(prev => prev - 1);
+        }
+    };
+
+    const stepLabels = {
+    1: "No Self-Assessment File Uploaded",
+    2: "Assessment Checking",
+    3: "No Action Plan Uploaded",
+    4: "Final Review"
+  };
+
   return (
-    <div className="dashboard-container">
+    <div className="user-dashboard-container">
       <header>
             <div className="leftside">
                 <div className="hamburger" onClick={toggleSidebar}>
@@ -34,7 +63,10 @@ export default function Udashboard() {
           <div className="sidebar-section">
             <p className="sidebar-label">HOME</p>
             <nav>
-              <div className="nav-item active">Dashboard</div>
+              <div className="nav-item active">
+                <img src={dashboardIcon} alt="Dashboard" width="25" height="25" className="deep-blue-filter"/>
+                Dashboard
+              </div>
             </nav>
           </div>
 
@@ -48,7 +80,44 @@ export default function Udashboard() {
         </aside>
 
         <main className="main-content">
-          <h1 id="main-content-title">Dashboard</h1>
+          <div className="main-content-header">
+            <h1 id="main-content-title">Welcome back, LGU AGENCY NAME</h1>
+            <button className="new-submission-btn">
+              <img src={addCircleIcon} alt="Add" width="25" height="25" className='white-filter'/>
+              Add Assessment
+            </button>
+          </div>
+          
+          <div className="main-content-container">
+            <div className="stat-tracker stat-container">
+              <div className="tracker">
+                <div className="tracker-steps">
+                  <span className={`circle ${currentStep >= 1 ? 'active' : ''}`}>1</span>
+                  <span className={`circle ${currentStep >= 2 ? 'active' : ''}`}>2</span>
+                  <span className={`circle ${currentStep >= 3 ? 'active' : ''}`}>3</span>
+                  <span className={`circle ${currentStep >= 4 ? 'active' : ''}`}>4</span>
+                  <div className="progress-bar">
+                    <span 
+                      className="indicator" 
+                      style={{ width: `${progressWidth}%` }}
+                    ></span>
+                  </div>
+                </div>
+                <p>Assessment Progress: <span className="progress-status"> {stepLabels[currentStep]}</span></p>
+                {/* Dev Button Functionality for Testing */}
+                
+                {/* <div className="tracker-debug-buttons">
+                  <button id="prev" onClick={handlePrev} disabled={currentStep === 1}>
+                    Previous
+                  </button>
+                  <button id="next" onClick={handleNext} disabled={currentStep === totalSteps}>
+                    Next
+                  </button>
+                </div> */} 
+                
+              </div>
+            </div>
+          </div>
         </main>
       </div>
     </div>
